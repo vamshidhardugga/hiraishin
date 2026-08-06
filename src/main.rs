d036bb03b3ec0837ca0e69f96865d56966178b54
Vamshidhar Reddy Dugga <235620241+vamshidhardugga@users.noreply.github.com>
2026-08-06 17:28:34 +0530

    feat(tracing): integrate tracing and JSON logging
    - Added `tracing` and `tracing-subscriber` dependencies to enhance logging
    - Configured `tracing-subscriber` for JSON-formatted log output with file, line number, and target
    - Updated `src/main.rs` to initialize tracing with INFO level logging
    - Added logging in the root route handler for better observability

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use axum::{Router, routing::get, serve};
 use std::env;
 use tokio::net::TcpListener;
+use tracing::info;
+use tracing_subscriber::{filter::LevelFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
     let port = env::var("PORT").expect("PORT environment variable must be set");
+    tracing_subscriber::registry()
+        .with(LevelFilter::INFO)
+        .with(
+            tracing_subscriber::fmt::layer()
+                .json()
+                .with_file(true)
+                .with_line_number(true)
+                .with_target(true)
+                .flatten_event(true),
+        )
+        .init();
     let app = Router::new().route("/", get(index));
     let listener = TcpListener::bind(format!("127.0.0.1:{}", port))
         .await
@@ -13,5 +26,6 @@ async fn main() {
 }
 
 async fn index() -> &'static str {
+    info!("Hiraishin - Redirection Engine");
     "Hiraishin - Redirection Engine"
 }
